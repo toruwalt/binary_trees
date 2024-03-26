e3b804a9df0e0a51962598e19b26f28566935383
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-26 01:49:55 +0000

    Binary Tree: Task 6:Different print method

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,9 +11,9 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
+	if (tree != NULL && func)
 	{
-		printf("%d \n", tree->n);
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
